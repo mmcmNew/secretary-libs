c375521d856bff9cb190e2ad5f7ba17a55fd58e3
mmcmNew <mmcmNew@users.noreply.github.com>
2025-09-05 15:29:15 +0000

    fix: Add ColorPicker in release

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './components/TaskTypeManager/TaskTypeManager';
 // TaskTypeDialog экспортируется для кастомизации, если потребуется
 export { default as TaskTypeDialog } from './components/TaskTypeManager/TaskTypeDialog';
 export * from './types';
+export { default as ColorPicker } from './components/ColorPicker/ColorPicker';
